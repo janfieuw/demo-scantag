ef0e7d8f4134ea99a370bca6c3bbbba3236096a6
Jan Fieuw <jan@docc.be>
2026-01-18 07:45:09 +0100

    BOLD

--- src/ui/layout.js
+++ src/ui/layout.js
@@ -21,7 +21,8 @@ function layout(title, bodyHtml) {
   <!-- JetBrains Mono (Google Fonts) -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet">
+
 
   <link rel="stylesheet" href="/static/base.css" />
 </head>

7f1a3d51563c7dda21662b8eacd38fa889a8751f
Jan Fieuw <jan@docc.be>
2026-01-21 03:13:12 +0100

    demo

--- src/app.js
+++ src/app.js
@@ -1,55 +1,43 @@
 // app.js
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
+// Bestaande routes (die jij al hebt)
+const accountRouter = require("./routes/account");
+const wizardRouter = require("./routes/wizard");
+const adminRouter = require("./routes/admin");
+
+// Nieuwe routes (die we net toegevoegd hebben)
 const reportsRouter = require("./routes/reports");
 const scanRouter = require("./routes/scan");
-
-// Als je nog andere routers hebt, voeg ze hier toe:
-// const demoAccountRouter = require("./routes/account");
-// const wizardRouter = require("./routes/wizard");
 
 function createApp() {
   const app = express();
 
-  /* ------------------------
-     Middleware
-  ------------------------ */
+  // Middleware
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
 
-  /* ------------------------
-     Static files
-  ------------------------ */
-  app.use("/static", express.static("public"));
+  // Static files: zorg dat /static/* werkt (base.css, demo.css, afbeeldingen, ...)
+  // Pas deze folder aan als jouw assets niet in ./public staan.
+  app.use("/static", express.static(path.join(__dirname, "public")));
 
-  /* ------------------------
-     Home
-  ------------------------ */
+  // Home -> start flow
   app.get("/", (req, res) => {
-    // ✅ meest logische entrypoint: account eerst
     return res.redirect("/demo/account");
-
-    // alternatief:
-    // return res.redirect("/wizard/company");
   });
 
-  /* ------------------------
-     Routes
-  ------------------------ */
+  // Routes mounten (BELANGRIJK)
+  app.use(accountRouter);
+  app.use(wizardRouter);
+  app.use(adminRouter);
 
-  // Jouw bestaande routers (zorg dat deze files bestaan)
   app.use(reportsRouter);
   app.use(scanRouter);
 
-  // Als je account/wizard routers hebt, mount ze ook:
-  // app.use(demoAccountRouter);
-  // app.use(wizardRouter);
-
-  /* ------------------------
-     404
-  ------------------------ */
+  // 404 (altijd als laatste)
   app.use((req, res) => {
     res.status(404).send("Not found");
   });

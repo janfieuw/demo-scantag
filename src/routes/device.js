32c4254d94d0ece2b2990e8d4de8e863fb9c1348
Jan Fieuw <jan@docc.be>
2026-02-01 15:51:05 +0100

    geslaagd

--- src/routes/device.js
+++ src/routes/device.js
@@ -239,8 +239,8 @@ function renderPairPage(tag, direction) {
 <body>
   <div class="wrap">
     <img class="logo" src="/static/logo_punctoo_groot_opgeel.png" alt="Punctoo" />
-    <div class="title">SCAN FOUTIEF.</div>
-    
+    <div class="title">KOPPELEN SMARTPHONE</div>
+    <div class="subtitle">Geef éénmalig ID:</div>
 
     <form method="POST" action="/pair">
       <input type="hidden" name="tagId" value="${tag.tag_id}" />
